8bbad57d73c0d7d57aa9dafd55eea60348e0abb4
MaorBitonDor <bitondor@post.bgu.ac.il>
2024-05-05 12:49:04 +0300

    Adding example file for the tabular data algorithm and the victim model files for the example

--- example_tabular.py
+++ example_tabular.py
@@ -1,33 +1,12 @@
 from Config import Config
-from TabularModels import AdultModel, RTIoTModel
+from TabularModels import RTIoTModel
 from Utility import (
     BAM_main_algorithm_tabular,
-    generate_random_tabular_data_adults,
     prepare_config_and_log, generate_random_tabular_data_rt_iot,
 )
 if __name__ == '__main__':
     prepare_config_and_log()
     config = Config.instance
-
-    # model_path = "adult_xgb_model.json"
-    # preprocessor_path = "adult_preprocessor.pkl"
-    #
-    # loaded_model = AdultModel()
-    # loaded_model.load(model_path, preprocessor_path)
-    #
-    # loaded_model.test_model()
-    #
-    # BAM_main_algorithm_tabular(
-    #     loaded_model,
-    #     AdultModel,
-    #     generate_random_tabular_data,
-    #     num_of_classes=2,
-    #     k=300,
-    #     epsilon=0.1,
-    #     population_size=1000,
-    #     generations=10,
-    #     search_spread=10,
-    # )
 
     model_path = "rt_iot_xgb_model.json"
     preprocessor_path = "rt_iot_preprocessor.pkl"
